ee3e6e0fddd2c989d8fd4c471ba298c6aae460bc
Sam Poppe <poppe.samuel@gmail.com>
2020-11-06 23:58:13 -0800

    adding cards for the deck

--- sheepshead/src/pages/Game/index.js
+++ sheepshead/src/pages/Game/index.js
@@ -2,3 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Hand from '../../components/Hand';
 import Card from '../../components/Card';
 
+const Game = () =>{
+  const [getCards, setCards] = useState([]);
+  const [getScores, setScores] = useState([]);
+
+  useEffect(() => {
+    setCards([
+      cards
+    ])
+  })
+}
+
+export default Game;

90409236223c89f5957c082d4bb1547df5705959
Sam Poppe <poppe.samuel@gmail.com>
2020-11-09 20:56:50 -0800

    trying to fix error with game page

--- sheepshead/src/pages/Game/index.js
+++ sheepshead/src/pages/Game/index.js
@@ -3,7 +3,7 @@ import Hand from '../../components/Hand';
 import Card from '../../components/Card';
 import cards from '../../assets/cards';
 
-const Game = () =>{
+function Game(){
   const [getCards, setCards] = useState([]);
   const [getScores, setScores] = useState([]);
 
@@ -11,9 +11,26 @@ const Game = () =>{
     setCards([
       cards
     ])
+
+    setScores([0,0,0,0,0])
   })
 
+  let player1Hand = getCards.slice(0,6);
+  let player2Hand = getCards.slice(6,12);
+  let player3Hand = getCards.slice(12,18);
+  let player4Hand = getCards.slice(18,24);
+  let player5Hand = getCards.slice(24,30);
   
+
+  return (
+    <div>
+      Player 1 hand: {player1Hand}
+      Player 2 hand: {player2Hand}
+      Player 3 hand: {player3Hand}
+      Player 4 hand: {player4Hand}
+      Player 5 hand: {player5Hand}
+    </div>
+  )
 }
 
 export default Game;
